aaee08b555101db0219fb8dea107f351fd93c401
BokkyPooBah <bokky.poobah@bokconsulting.com.au>
2021-09-19 18:57:55 +1000

    Testing

---
+++ docs/js/three.js
@@ -0,0 +1,10 @@
+/*
+ * Skypack CDN - three@0.132.2
+ *
+ * 📙 Package Documentation: https://www.skypack.dev/view/three
+ * 📘 Skypack Documentation: https://www.skypack.dev/docs
+ */
+
+// Browser-Optimized Imports (Don't directly import the URLs below in your application!)
+export * from '/-/three@v0.132.2-1edwuDlviJO0abBvWgKd/dist=es2020,mode=imports/optimized/three.js';
+export {default} from '/-/three@v0.132.2-1edwuDlviJO0abBvWgKd/dist=es2020,mode=imports/optimized/three.js';
